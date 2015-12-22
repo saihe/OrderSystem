234fa1956b84d3617f3cc59a786b2a7bc78a74c3
櫻畑友里 <blackbutterfly.0929@gmail.com>
2015-12-22 13:51:44 +0900

    test,フロントのみ

---
+++ system/accounting.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>accounting</title>
+</head>
+<body>
+	<p>会計するよ</p>
+	
+	<br>
+	<a href="order.php">注文する</a>
+</body>
+</html>
---
+++ system/operation.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>operation</title>
+</head>
+<body>
+	<p>ログインできたよ</p>
+	<p>操作どうする？</p>
+	<a href="order.php">注文する</a>
+	<br>
+	<a href="accounting.php">会計する</a>
+</body>
+</html>
