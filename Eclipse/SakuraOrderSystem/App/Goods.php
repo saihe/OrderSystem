3e85978ea3087b90aa902d5d9a95e1ae2a3c543a
saihe <saihe.kyon@gmail.com>
2015-12-23 22:39:02 +0900

    商品一覧の出力と商品番号から商品を検索するメソッドを追加。

--- Eclipse/SakuraOrderSystem/App/Goods.php
+++ Eclipse/SakuraOrderSystem/App/Goods.php
@@ -12,8 +12,15 @@
 			"D002" => array("ウーロン茶", 200),
 	);
 
+	//商品番号から商品情報を検索
 	function getGoods($goods, $goodsNo){
 		return $goods[$goodsNo][0] . "\t" . $goods[$goodsNo][1] . "円";
 	}
 
+	//商品一覧をテーブルに表示し、ボタンを押すと商品が注文リストに追加される
+	function getAllGoods($goods){
+		foreach ($goods as $val){
+			print "<tr><td>" . $val[0] . "</td><td>" . $val[1] . "円</td><td><input type='button' value='追加'></td></tr>";
+		}
+	}
 ?>
--- Eclipse/SakuraOrderSystem/App/Operation.php
+++ Eclipse/SakuraOrderSystem/App/Operation.php
@@ -4,6 +4,7 @@ require_once 'Goods.php';
 
 $res = "";
 
+//注文する
 if (isset ( $_POST ["submit"] )) {
 	// GoodsクラスのgetGoods()を使う
 	$res = getGoods ( $allGoods, $_POST["goodsNo"] );
@@ -15,10 +16,21 @@ if (isset ( $_POST ["submit"] )) {
 <head>
 </head>
 <body>
+<!-- 商品一覧 -->
+	<table >
+		<tr>
+			<th>商品名</th>
+			<th>値段</th>
+		</tr>
+		<?php getAllGoods($allGoods)?>
+	</table>
+
 	<form method="post" action="">
-		<input type="text" name="goodsNo">
 		 <input type="submit" name="submit" value="送信">
 	</form>
+
+<!-- 注文商品出力 -->
 	<p><?php print $res; ?></p>
+
 </body>
 </html>
