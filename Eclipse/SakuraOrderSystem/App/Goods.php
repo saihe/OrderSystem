1229c336e49589adfdfad2bf3fa361874e334820
saihe <saihe.kyon@gmail.com>
2015-12-23 22:40:25 +0900

    コメント編集。

--- Eclipse/SakuraOrderSystem/App/Goods.php
+++ Eclipse/SakuraOrderSystem/App/Goods.php
@@ -17,7 +17,7 @@
 		return $goods[$goodsNo][0] . "\t" . $goods[$goodsNo][1] . "円";
 	}
 
-	//商品一覧をテーブルに表示し、ボタンを押すと商品が注文リストに追加される
+	//商品一覧をテーブルに表示し、ボタンを押すと商品が注文リストに追加されるようにしたい
 	function getAllGoods($goods){
 		foreach ($goods as $val){
 			print "<tr><td>" . $val[0] . "</td><td>" . $val[1] . "円</td><td><input type='button' value='追加'></td></tr>";
