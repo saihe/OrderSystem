c662535222965cd1f2cb94bd0e82bbf5779df8eb
saihe <saihe.kyon@gmail.com>
2016-03-27 22:20:13 +0900

    データベース作成。ダンプ。 ドキュメント更新。

---
+++ lib/ordersystem.sql
@@ -0,0 +1,109 @@
+-- phpMyAdmin SQL Dump
+-- version 4.3.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 2016 年 3 朁E27 日 15:17
+-- サーバのバージョン： 5.6.24
+-- PHP Version: 5.6.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `ordersystem`
+--
+CREATE DATABASE IF NOT EXISTS `ordersystem` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `ordersystem`;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `customers`
+--
+
+DROP TABLE IF EXISTS `customers`;
+CREATE TABLE IF NOT EXISTS `customers` (
+  `id` varchar(32) NOT NULL DEFAULT '' COMMENT 'yyyymmdd_hhmm_[0-9[0-9]',
+  `number` int(20) NOT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `historieds`
+--
+
+DROP TABLE IF EXISTS `historieds`;
+CREATE TABLE IF NOT EXISTS `historieds` (
+  `id` int(255) NOT NULL,
+  `customer_id` varchar(32) DEFAULT NULL COMMENT 'yyyymmdd_hhmm_[0-9][0-9]',
+  `product_id` int(128) DEFAULT NULL,
+  `amount` int(20) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE IF NOT EXISTS `products` (
+  `id` int(128) NOT NULL,
+  `cord` varchar(8) NOT NULL DEFAULT '',
+  `name` varchar(128) DEFAULT NULL,
+  `unit_price` int(6) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `historieds`
+--
+ALTER TABLE `historieds`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`,`cord`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `historieds`
+--
+ALTER TABLE `historieds`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(128) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
